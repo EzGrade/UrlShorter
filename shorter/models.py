f09bb38250cb05fdd9ef01a204e736bbc84f036c
Grade <aborecevskij94@gmail.com>
2024-07-28 16:54:22 +0300

    upd: made short url not editable

--- shorter/models.py
+++ shorter/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class URL(models.Model):
-    short_url = models.CharField(max_length=16)
+    short_url = models.CharField(max_length=16, unique=True, editable=False)
     initial_url = models.URLField()
     creation_date = models.DateTimeField(auto_now_add=True)
     auto_delete_date = models.DateTimeField(null=True, blank=True)
